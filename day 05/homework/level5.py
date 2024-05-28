2c984af31884df2bf0ce05d403a4164f73b3e949
Rezzikoo <batutadadusi@gmail.com>
2024-05-28 18:01:07 +0400

    fifth commit

---
+++ day 05/homework/level5.py
@@ -0,0 +1,38 @@
+#my information
+name = "Rezi"
+surname = "Gvaramia"
+age = 15
+address = "Tbilisi"
+
+#display information
+print("name")
+print("surname")
+print("age")
+print("address")
+
+
+#create variables
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
+f = 6
+g = 7
+h = 8
+i = 9
+j =10
+
+#addition
+addition_results = a+b+c+d+e+f+g+h+i+j 
+# #substruction
+substruction_results = a-b-c-d-e-g-f-h-j-i
+# #division
+division_results = a/b/c/d/e/f/g/h/i/j
+# #multiplication
+multiplication_results = a*b*c*d*e*f*g*h*i*j
+# #display results
+print(addition_results)
+print(substruction_results)
+print(division_results)
+print(multiplication_results)
