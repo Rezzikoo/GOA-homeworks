5188511468fcadcfcdc260d5449939f79757d0c9
Rezzikoo <batutadadusi@gmail.com>
2024-05-28 21:03:41 +0400

    commit

---
+++ day 06/classwork/level6class2.py
@@ -0,0 +1,6 @@
+firstname=input("enter your first name")
+lastname=input("enter your last name")
+age=int(input("enter your age"))
+email=input("enter your email")
+
+print(firstname + lastname + age + email)
