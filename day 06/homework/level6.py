c5716958139fe864b143906a2b6e988c4ce94363
Rezzikoo <batutadadusi@gmail.com>
2024-06-01 14:10:04 +0400

    commit

---
+++ day 06/homework/level6.py
@@ -0,0 +1,30 @@
+#creating inputs
+
+#a=input()
+#b=input()
+
+#mathematical operations
+
+#print(int(a)+int(b))
+#print(int(a)-int(b))
+#print(int(a)*int(b))
+#print(int(a)/int(b))
+
+#creating input
+
+#name=input()
+#printing message
+#print("congradulations!"+" "+ name)
+
+
+#creating inputs
+
+first_name=input()
+last_name=input()
+age=input()
+
+#printing the information
+
+print(first_name+" "+ last_name+" "+ int(age))
+
+
