14b44b64d18329adfb6f8616a3a5abcc7d49c0ec
Rezzikoo <batutadadusi@gmail.com>
2024-06-04 20:06:54 +0400

    commit

---
+++ day 08/claswork/level8.py
@@ -0,0 +1,14 @@
+password1=input("please enter your password")
+password2=input("please repeat your password")
+
+print(password1==password2)
+
+
+#num1=int(input())
+#num2=int(input())
+
+#print(num1>num2)
+#print(num1<num2)
+#print(num1>=num2)
+#print(num1<=num2)
+#print(num1=num2)
