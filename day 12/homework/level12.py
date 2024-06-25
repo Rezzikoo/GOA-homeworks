c177a9da2f882e254ca365c71c228f875f25c90b
Rezzikoo <batutadadusi@gmail.com>
2024-06-25 12:50:29 +0400

    commit

---
+++ day 12/homework/level12.py
@@ -0,0 +1,39 @@
+#for i in range(11):
+    #print(i)
+
+
+#for i in range(3):
+    #print("hello World")
+#number=5
+#while number > 0:
+    #print(number)
+    #number=number-1
+
+#i=3
+#while i >0:
+   #print("Hello")
+   #i=i-1
+
+
+
+#age=int(input())
+#if age>0:
+    #print("true")
+#else:
+    #print("false")
+
+
+#number=int(input())
+#if number==10:
+    #print("true")
+#else:
+    #print("false")
+
+
+
+age=input()
+if age>0 and age<10:
+    print("child")
+
+    
+
