0ed5fca4d1197508656a7015865144eb4e20e939
Rezzikoo <batutadadusi@gmail.com>
2024-06-29 13:08:51 +0400

    commit

---
+++ day 14/classwork/level14cw.py
@@ -0,0 +1,26 @@
+#for i in range(2,11,2):
+    #print(i)
+
+
+
+
+
+
+#for i in range(1,10,2):
+    #print(i)
+
+
+
+#for i in range(5,100,5):
+    #print(i)
+
+
+
+#for i in range(3,100,3):
+    #print(i)
+
+
+
+for i in range(20,0,-1):
+    print(i)
+
---
+++ day 14/classwork/level14cw2.py
@@ -0,0 +1,17 @@
+#password=...
+#guess=input()
+#while guess!=password:
+    #print("try again")
+#else:
+    #print("success")
+
+
+sum=0
+i=0
+while i<+10:
+    print(i)
+    i=i+2
+    sum=sum+i
+    print(sum)
+
+   
