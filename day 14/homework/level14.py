8f4f70b9b27f4d94a71275489b022dfd3b5eb0a1
Rezzikoo <batutadadusi@gmail.com>
2024-07-02 13:07:22 +0400

    commit

---
+++ day 14/homework/level14.py
@@ -0,0 +1,73 @@
+# for i in range(21):
+#     print(i)
+
+# for i in range(11):
+#     print(i)
+
+# for i in range(0,100+1,2):
+#     print(i)
+
+# for i in range(0,100+1,3):
+#     print(i)
+
+# for i in range(0,50+1,5):
+#     print(i)
+
+
+# i=0
+# while i <20:
+#     print(i)
+#     i=i+1
+
+# sum=0
+# i=0
+# while i<10:
+#     print(i)
+#     i=i+1
+#     sum=sum+i
+# print(sum) #???
+
+# correct=7
+# guess=input()
+# while guess!=correct:
+#     print("try again")
+# if guess==correct:
+#     print("good job") #???
+
+# i=0
+# while i<100:
+#     print(i)
+#     i=i*2 #???
+
+# password="password123"
+# guess=input()
+# while guess!=password:
+#     print("try again")
+# if guess==password:
+#     print("good job") #???
+
+# time=13
+# if time<12:
+#     print("good morning")
+# else:
+#     print("good afternoon")
+
+# number=3
+# if number / 2:
+#     print("even")
+# else: 
+#print("odd") #???
+
+# temp=31
+# if temp>30:
+#     print("its hot outside")
+# else:
+#     print("its not too hot outside")
+
+# age=20
+# if age>13:
+#     elif age<19:
+#     print("you are a teenager")
+# else:
+#     print("you are not a teenager") #???
+  
