e91f48871a9fe540a11ea71c9f6ad3aa23f6a385
Rezzikoo <batutadadusi@gmail.com>
2024-07-06 14:34:46 +0400

    commit

---
+++ day 16/classwork/level16cw.py
@@ -0,0 +1,34 @@
+#names=["sandro","nika","nika","saba","saba"]
+# print(names[0])
+# print(names[2])
+# print(names[4])
+
+# names[2]="blabla"
+# names[3]="blabla"
+# names[4]="blabla"
+# print(names)
+
+# colors=["red","white","blue","green","orange"]
+# colors.append("blabla")
+# colors.pop(0)
+# print(colors)
+
+
+# cars=[]
+# for i in range(5):
+#     addcars=input()
+#     cars.append(addcars)
+# print(cars)
+
+colors=["red","white","blue","green","orange","orange","orange","orange"]
+
+# index=colors.index("blue")
+# print(index)
+
+# count=colors.count("orange")
+# print(count)
+
+len=len(colors)
+print(len)
+
+
