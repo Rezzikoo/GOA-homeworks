3ad77453d742a946813235cfa982dea4592623dc
Rezzikoo <batutadadusi@gmail.com>
2024-07-09 15:26:58 +0400

    commit

---
+++ day 16/homework/level16.py
@@ -0,0 +1,37 @@
+# integers=[1,2,3,4]
+# integers.pop(3)
+# print(integers)
+
+# strings=["bla","lab","bal"]
+# strings.pop(0)
+# strings.append(0)
+
+# characters=["a","b","c"]
+# characters.pop(2)
+
+# blabla=[1,2,3,4,5]
+# blabla.pop(3)
+# blabla.append(3)
+
+# integers=[1,2,3,4,5,5,6]
+# count=integers.count(5)
+# print(count)
+
+# strings=["a","a","a","b"]
+# count=strings.count("a")
+# print(count)
+
+# blabla=[1.2,1.4,2,4.1]
+# count=blabla.count(int())
+# print(count) #?
+
+# blabla=[1,2,3[3,4],5,6]
+# count=blabla.count([])
+# print(count) #?
+
+integers=[1,2,3,4,5,6,7,8,9,10]
+print(len(integers))
+
+
+
+
