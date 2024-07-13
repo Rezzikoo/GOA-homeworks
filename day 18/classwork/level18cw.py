20c07c7bf2fabb4da3d196be75bdcac79f4e35b2
Rezzikoo <batutadadusi@gmail.com>
2024-07-13 12:55:22 +0400

    commit

---
+++ day 18/classwork/level18cw.py
@@ -0,0 +1,59 @@
+#num=[1,2,3,4,5,6,7,8]
+#num_example=num[0:5]
+#print(num_example)
+#num_example=num[4:7]
+#print(num_example)
+# num_example=num[0:10:2]
+# print(num_example)
+# num_example=num[4:7]
+# print(num_example)
+# num_example=num[0:4]
+# print(num_example)
+# num_example=num[4:8]
+# print(num_example)
+# num_example=num[0:8:7]
+# print(num_example)
+# num_example=num[2:8:2]
+# print(num_example)
+
+# string="yellow"
+# example_string=string[3:6]
+# print(example_string)
+
+# example="red"
+# example_1=example[::-1]
+# print(example_1)
+
+# example="yellow"
+# example_1=example[::-1]
+# example_2=example_1[0::2]
+# print(example_2)
+
+#example=[1,2,3,4,5,6,7,8,9]
+# example_copy=example
+# example_copy[0]="new_example"
+# print(example)
+# print(example_copy)
+# example.clear()#ასუფთავებს მთლიან სიას
+
+
+
+# list_1=[1,2,3,4]
+# list_2=[5,6,7,8]
+
+# list_1.extend(list_2)
+# print(list_1)
+
+# list_1=["red","yellow","green"]
+# list_1.insert(1,"brown")
+# print(list_1)
+
+
+
+
+
+
+
+
+
+
