fb7bc8612f3b6130c8d63d22fc71da0a43549492
Rezzikoo <batutadadusi@gmail.com>
2024-07-16 14:24:54 +0400

    commit

---
+++ day 18/homework/level18.py
@@ -0,0 +1,29 @@
+# nums=[1,2,3,4,5,6]
+# nums=nums[1:5]
+# print(nums)
+
+# strings=["red","brown","green"]
+# integers=[1,2,3,]
+# new_list=[strings[0:1:]+integers[0:1:]+strings[1:2]+integers[1:2]+strings[2:3]+integers[2:3]]
+# print(new_list)
+
+# double_list=[1,2,3,"red","green","brown"]
+# integers_list=double_list[0:3]
+# strings_list=double_list[3:6]
+# print(integers_list)
+# print(strings_list)
+
+# list_1=[1,2,3,4]
+# list_2=[5,6,7,8]
+# example_list1=list_1[0:3:2]
+# example_list2=list_2[0:3:2]
+# example_list3=list_1[1:4:2]
+# example_list4=list_2[1:4:2]
+# odd_list=example_list1+example_list2
+# even_list=example_list3+example_list4
+# print(odd_list)
+# print(even_list) 
+
+list=[1,2,3,4,5,6,7,8]
+even_list=list[::2]
+print(even_list)
