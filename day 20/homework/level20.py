1abb4d9568149522827f1148b70e5695e0065ed7
Rezzikoo <batutadadusi@gmail.com>
2024-07-23 15:03:28 +0400

    commit

---
+++ day 20/homework/level20.py
@@ -0,0 +1,34 @@
+# def sum_numbers():
+#     sum=0
+#     numbers=[1,2,3,4,5]
+#     even_numbers=numbers[1:10:2]
+#     sum=sum+numbers
+# sum_numbers() #???
+
+# def reverse_string():
+#     str="red"
+#     reversed_string=str[::-1]
+#     print(reversed_string)
+# reverse_string()
+
+
+# def ascending_order():
+#     integers=[5,4,3,2,1]
+#     ascending_order=integers[::-1]
+#     print(ascending_order)
+# ascending_order()
+
+def prime_number():
+    example_number=[2]
+    random_number=5
+    if random_number/example_number:
+        print("false")
+    else:   
+
+        print("True")
+prime_number()
+
+    
+
+
+
