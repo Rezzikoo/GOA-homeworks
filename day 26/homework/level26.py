87d21cb4539ff12aaa946e7b362b51548d43ba62
Rezzikoo <batutadadusi@gmail.com>
2024-08-09 22:42:49 +0400

    commit

---
+++ day 26/homework/level26.py
@@ -0,0 +1,15 @@
+string="Example"
+string=string.upper()
+print(string)   #all the characters are in uppercase
+
+
+string="Example"
+string=string.lower()
+print(string)   #all the characters are in lowercase
+
+
+string="example"
+string=string.capitalize()
+print(string)   #only the first letter is in uppercase
+
+
