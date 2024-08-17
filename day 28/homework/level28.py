85cc432772553685c9063025dc8325b3c09d41c5
Rezzikoo <batutadadusi@gmail.com>
2024-08-17 12:17:31 +0400

    commit

---
+++ day 28/homework/level28.py
@@ -0,0 +1,38 @@
+#In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+# def well(x):
+#     if x.count("good")<=2:
+#         return "Publish!"
+#     if x.count("good")==0:
+#         return "Fail!"
+#     else:
+#         return "I smell a series!"
+
+
+#Complete the square sum function so that it squares each number passed into it and then sums the results together.
+#For example, for [1, 2, 2] it should return 9 because 
+# def square_sum(numbers):
+#     sum=0
+#     for i in numbers:
+#         sum=sum+i*i
+#     return sum
+
+
+#Given an array of integers your solution should find the smallest integer.
+# def find_smallest_int(arr):
+#     min_num=arr[0]
+#     for i in arr:
+#         if i<min_num:
+#             min_num=i
+#     return min_num
+
+
+#You get an array of numbers, return the sum of all of the positives ones.
+# def positive_sum(arr):
+#     sum=0
+#     for i in arr:
+#         if i>0:
+#             sum=sum+i
+#     return sum
+        
+
+
