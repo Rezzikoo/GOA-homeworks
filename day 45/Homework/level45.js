5a165d59f104d9eeccaa51295f5d3e86810d8ab8
Rezzikoo <batutadadusi@gmail.com>
2024-10-27 14:49:10 +0400

    commit

---
+++ day 45/Homework/level45.js
@@ -0,0 +1,8 @@
+let p1=Document.GetElementById(p1)
+ p1.style.color="purple"
+ p1.text.content="Hello world"
+
+ 
+
+
+
---
+++ day 47/Homework/level47.js
@@ -0,0 +1,7 @@
+if(100>50) {
+    console.log("true")
+}else if (100>10){
+    console.log("true")
+}else{
+    console.log("false")
+}
