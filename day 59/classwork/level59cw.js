9234d0f4adfef9a91d3861161600db6090b5705f
Rezzikoo <batutadadusi@gmail.com>
2024-11-30 22:06:35 +0400

    commit'

---
+++ day 59/classwork/level59cw.js
@@ -0,0 +1,18 @@
+// a=5
+// b=7
+// c=39
+// d=9198
+// g=239874893
+// console.log(a>b)
+// console.log(c==d)
+// console.log(b>g)
+// console.log(d==a)
+// console.log(c==b)
+
+// console.log(c>b)
+// console.log(a>b)
+// console.log(c<d)
+// console.log(b<g)
+// console.log(d>a)
+
+
