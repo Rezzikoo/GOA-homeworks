36e924c593d69456f72d8c3a62c8e16c87d0bb9f
Rezzikoo <batutadadusi@gmail.com>
2024-12-28 23:08:52 +0400

    commit

--- day 64/homework/level64.py
+++ day 64/homework/level64.py
@@ -1,6 +1,77 @@
-def can_we_go_outside(good_weather, good_time, go_out):
+# def can_we_go_outside(good_weather, good_time, go_out):
     
-    if (good_weather or good_time) and go_out:
-        return True
-    return False
+#     if (good_weather or good_time) and go_out:
+#         return True
+#     return False
+
+# def take_a_step(barrier, space, signal_from_other_side):
+#     if not barrier and space:
+#         return True
+#     elif signal_from_other_side:
+#         return True
+#     else:
+#         return False
+
+
+# def should_we_take_the_item(item_available, item_allowed, other_person_wants_to_use):
+#     if item_available and item_allowed:
+#         return True
+#     elif not other_person_wants_to_use:
+#         return True
+#     else:
+#         return False
+
+
+# def should_we_recall_information( current_task: , information_needed: , time_to_process: ):
+#     if current_task and information_needed:
+#         return True
+#     elif time_to_process:
+#         return True
+#     else:
+#         return False
+
+
+# def sleep(late:, tired:, need_to_get_up_early: , lights_off:):
+#     if late and person_is_tired:
+#         return True
+#     elif need_to_get_up_early and lights_off:
+#         return True
+#     else:
+#         return False
+
+# def go_to_the_party(friends_agree:, no_work_next_day: , party_nearby: ):
+#     if friends_agree and no_work_next_day:
+#         return True
+#     elif party_nearby:
+#         return True
+#     else:
+#         return False
+
+# def stay_at_home(is_rainy: , is_snowy:, no_plans_outside:):
+    
+#     if (is_rainy or is_snowy) and no_plans_outside:
+#         return True
+#     else:
+#         return False
+
+
+
+
+
+
+# def go_for_a_run_or_not(is_dry: , shoes_available: , friend_willing_to_run:): 
+#     if (is_dry and shoes_available) or friend_willing_to_run:
+#         return True
+#     else:
+#         return False
+
+
+# def watch_football(is_saturday:, is_sunday:, no_other_things_to_do:, favorite_team_playing:):
+#     if (is_saturday or is_sunday) and no_other_things_to_do:
+#         return True
+#     elif favorite_team_playing:
+#         return True
+#     else:
+#         return False
+
 
