7c804f0d06a5760ec85b4368dd6a5c81ea9cd378
Rezzikoo <batutadadusi@gmail.com>
2025-03-01 12:24:12 +0400

    commit

---
+++ day 83/classwork/level83cw.py
@@ -0,0 +1,28 @@
+# # def traffic_light(current):
+# #      transitions = {          #ra feri ra fershi unda gadavides
+# #          "green": "yellow",
+# #          "yellow": "red",
+# #          "red": "green"
+# #      }
+# #      return transitions.get(current)  #abrunebs amjamindel fers da cvlis am fers mis shesababis transitionshi
+
+# # print(traffic_light("green"))  
+# # print(traffic_light("yellow"))
+# # print(traffic_light("red"))    
+
+
+
+
+
+
+
+# def set_alarm(employed, vacation):
+#      return employed and not vacation    #alarmi dayendeba tu xar momushave da ar xar shvebulebashi
+
+# print(set_alarm(True, True))
+# print(set_alarm(True, False))
+# print(set_alarm(False, True))
+# print(set_alarm(False, False))
+
+
+
